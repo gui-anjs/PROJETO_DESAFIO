a57e88442404160ce17420bb05f071cc8fb74209
João Guilherme <joaoguilhermedosanjos@gmail.com>
2026-06-10 21:18:24 -0300

    adicionado js

--- projeto git/projeto_desafio/js/script_fundamentos.js
+++ projeto git/projeto_desafio/js/script_fundamentos.js
@@ -7,12 +7,12 @@ comentário de múltiplas linhas é possível ter várias linhas
 
 
 //COMANDO DE SAÍDA 
-console.log("Olá mundo!!")
+console.log("JG O GAROTINHO")
 
 //DECLARAÇÃO DE VARIÁVEL
 let num = 10
 var num2 = 50 
-const num3 = 100
+let num3 = 100
 
 console.log(num)
 console.log(num2)
@@ -32,5 +32,49 @@ console.log(num)
 num2=58
 console.log(num2)
 
-num3 = 2021
+num3 = 100
 console.log(num3)
+
+//CONCATENAÇÃO
+
+console.log('valor da variável num3 é: ' + num3)
+console.log("valor da variável num3 é: " + num3)
+console.log("valor da variável num3 é: ", num3)
+
+//CONCATENAÇÃO TEMPLATE: acento grave, cifrão e chaves
+
+console.log(`valor da variável num3 é ${num3}`)
+
+//OPERADORES MATEMÁTICOS
+/*
+   + soma 
+   - subtração 
+   * multiplicação 
+   / divisão 
+   % módulo - resto da divisão
+*/
+console.log('A soma dos números é: ', 12+8)
+console.log('A subtração entre as variáveis é:', num3-num2)
+console.log('A multiplucação dos números é: ', num2*5)
+console.log('A divisão etres os valores é de: ', num3/5)
+console.log(`O resto da divisão entre 10 e 2 é: ${10 % 2}`)
+
+//OPERADORES RELACIONACIONAIS  OU COMPARAÇÃO
+/*
+
+   > MAIOR
+   < MENOR
+   >= MAIOR OU IGUAL
+   <= MENOR OU IGUAL
+   != DIFERENTE
+*/
+
+//OPERADORES LÓGICOS
+/*
+
+   && E
+   || OU 
+*/ 
+
+//TESTE LÓGICO 
+if()
